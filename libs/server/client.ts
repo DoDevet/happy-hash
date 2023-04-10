491beb07e42dcda3fb71e0b4e2a69914e846bf5b
DoDevet <ehwlgns000@gmail.com>
2023-04-10 22:05:04 +0900

    Deploy Test

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+declare global {
+  var client: PrismaClient | undefined;
+}
 
-const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: [],
-  });
-globalForPrisma.prisma = prisma;
-export default prisma;
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default client;

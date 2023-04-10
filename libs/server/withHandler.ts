2978787c059353ebdbf558f41fcb16cf9cc30315
DoDevet <ehwlgns000@gmail.com>
2023-04-10 21:47:48 +0900

    Deploy Test

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -29,7 +29,7 @@ export default function withHandler({
     try {
       await handler(req, res);
     } catch (error) {
-      res.status(500).json({ error });
+      res.status(505).json({ error });
     }
   };
 }
